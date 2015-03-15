e158d2a27b972fff9505b30aa4bda3bd5d829000
JFRuel <jf@arlequinade.com>
2015-03-15 17:54:46 -0400

    rprog-012 first modifications

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Computes inverse of matrix, then commits result and original matrix to cache
 
 makeCacheMatrix <- function(x = matrix()) {
 
+        original_matrix <<- x
+        output <<- solve(x)
+        return(output)
+        
 }
 
 
-## Write a short comment describing this function
+## computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+## If the inverse has already been calculated (and the matrix has not changed), 
+## then the cachesolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+        ## The if statement tests if 'x' is identical to the input matrix used in makeCacheMatrix()
+        ## There is no need to test if inverted matrix exist since we take for granted that the
+        ## matrix can be inverted.
+        
+        if (identical(x,original_matrix)){   
+                 return(output)               
+                 
+                
+        }else {makeCacheMatrix(x)}
+
 }
